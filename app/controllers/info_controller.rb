e9d3f9b9f42d12d064927d5ace2207f77cdb99a6
okorkov <alex.okorkov@gmail.com>
2021-05-25 15:10:28 -0700

    added image upload for projects and woods

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -5,6 +5,8 @@ class InfoController < ApplicationController
     info.update(info_params)
     info.header_img.attach(params[:info][:header_image]) if params[:info][:header_image]
     info.about_img.attach(params[:info][:about_img]) if params[:info][:about_img]
+    info.project_pics.attach(params[:info][:project_pics]) if params[:info][:project_pics]
+    info.wood_pics.attach(params[:info][:wood_pics]) if params[:info][:wood_pics]
     redirect_to root_path
   end
 
--- app/models/info.rb
+++ app/models/info.rb
@@ -2,5 +2,7 @@ class Info < ApplicationRecord
 
   has_one_attached :header_img
   has_one_attached :about_img
+  has_many_attached :project_pics
+  has_many_attached :wood_pics
 
 end
