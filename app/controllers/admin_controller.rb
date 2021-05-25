d64a2cb3c77d5de4aae3859e5550cf773ade12ba
okorkov <alex.okorkov@gmail.com>
2021-05-24 17:03:02 -0700

    sessions and autentication added

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,9 @@
+class AdminController < ApplicationController
+
+  before_action :require_login
+
+  def home
+    
+  end
+  
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def define_admin
+    @admin = current_user
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+   end
+ 
+   def require_login
+     redirect_to new_session_path if !current_user
+   end
+ 
+   def logged_in?
+     session[:user_id] != nil
+   end
+
 end
