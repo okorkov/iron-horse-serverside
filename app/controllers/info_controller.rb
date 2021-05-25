c3b9e9f0bb2fbb815efc3f0c447267e63f9a0391
okorkov <alex.okorkov@gmail.com>
2021-05-25 14:27:13 -0700

    added image for about category

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -4,6 +4,7 @@ class InfoController < ApplicationController
     info = Info.all.first
     info.update(info_params)
     info.header_img.attach(params[:info][:header_image]) if params[:info][:header_image]
+    info.about_img.attach(params[:info][:about_img]) if params[:info][:about_img]
     redirect_to root_path
   end
 
