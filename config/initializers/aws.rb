4055cd5143f396094853b699860377d4c64750bf
okorkov <alex.okorkov@gmail.com>
2021-05-25 18:50:07 -0700

    aws config #2

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,5 @@
-Aws.config.update({
-  region: 'us-west-1', # NOTE: Your region may differ
-  credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
-})
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
+# Aws.config.update({
+#   region: 'us-west-1', # NOTE: Your region may differ
+#   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+# })
+# S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
