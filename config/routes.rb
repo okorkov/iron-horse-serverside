0d0c6f4c1013c5ea84b9622dd27b0d6dd70909b7
okorkov <alex.okorkov@gmail.com>
2021-06-08 15:38:01 -0700

    removed header image from headers

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,14 @@ Rails.application.routes.draw do
   resources :info, only: [:update]
   resources :admin, only: [:index]
   resources :users, only: [:create, :destroy]
+  
+  get '/emails', to: 'emails#home'
 
   delete '/admin/delete_images_projects/:id', to: 'admin#delete_collections_projects', as: 'delete_image_attachment_collections_projects'
   delete '/admin/delete_images_wood/:id', to: 'admin#delete_collections_wood', as: 'delete_image_attachment_collections_wood'
 
   get '/api', to: 'api#data'
+
 
   match "*any", via: :all, to: "errors#not_found"
 
